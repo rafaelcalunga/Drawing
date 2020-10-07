a5a626558d0c97997b31e696f1a12be09d32b42a
Rafael Calunga <rafaelcalunga@gmail.com>
2020-10-06 20:12:27 -0400

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Rafael Calunga on 2020-10-06.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
